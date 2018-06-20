1654da61a900e94f3d5c64dfffb23091e808fcbc
VILLE Vinicius Lima <vville@lanet.accorservices.net>
2018-06-20 10:32:25 -0300

    alteracao de dependencia do spring boot e outros

--- delivery-order-service/src/main/java/com/coderef/delivery/security/SecurityConfiguration.java
+++ delivery-order-service/src/main/java/com/coderef/delivery/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.coderef.delivery.security;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
